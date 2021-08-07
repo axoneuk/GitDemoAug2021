0b790264dd0209f4d6202420646ca673f470b6de
ramduvvuri <ram.duvvuri@gmail.com>
2021-08-07 08:26:35 +0100

    Jira 1234 - Added unit tests

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Git is cool");
+        System.out.println("Git is cool ....");
     }
 
 }
--- src/test/java/Test_Application.java
+++ src/test/java/Test_Application.java
@@ -11,6 +11,10 @@ public class Test_Application {
     public void test2(){
         System.out.println("running test 2");
     }
-}
 
+    @Test
+    public void test3(){
+        System.out.println("running test 3");
+    }
+}
 
